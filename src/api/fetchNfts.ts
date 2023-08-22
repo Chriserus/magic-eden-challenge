fcc3cb330255b69fbbd7a9be4463cebcb3789898
Chriserus <krzysborowiecki97@gmail.com>
2023-08-22 17:57:28 +0200

    Remove usage of .env

--- src/api/fetchNfts.ts
+++ src/api/fetchNfts.ts
@@ -1,10 +1,9 @@
 import wrapPromise from './wrapPromise'
 import {ApiResponse, NFT} from "../common/types";
 
-const NFT_LISTINGS_ENDPOINT = process.env.REACT_APP_NFT_LISTINGS_ENDPOINT;
+const NFT_LISTINGS_ENDPOINT = "https://api-mainnet.magiceden.io/idxv2/getListedNftsByCollectionSymbol?collectionSymbol=okay_bears&limit=10000&offset=0";
 
 function fetchNfts(): { read: () => NFT[] } {
-  if (!NFT_LISTINGS_ENDPOINT) throw new Error('REACT_APP_NFT_LISTINGS_ENDPOINT not set')
   const promise = fetch(NFT_LISTINGS_ENDPOINT)
     .then(data => data.json())
     .then((apiResponse: ApiResponse) => apiResponse.results)
